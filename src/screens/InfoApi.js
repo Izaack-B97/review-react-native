3be723dddba252b1975d7b4fa0189a3a1161477d
Isaac Bustamante <izaack.97@gmail.com>
2021-08-15 19:02:59 -0700

    Added flastlist

--- src/screens/InfoApi.js
+++ src/screens/InfoApi.js
@@ -1,5 +1,6 @@
+import moment from 'moment';
 import React, { useState, useEffect } from 'react'
-import { ScrollView, View, Text, Button } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const InfoApi = ({ navigation }) => {
@@ -14,13 +15,85 @@ export const InfoApi = ({ navigation }) => {
             .catch( err => console.log( err ) );
     }, [])
 
-    console.log('Sobres ps')
+    const renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity
+                activeOpacity={ 0.8 }
+            >
+                <View
+                    style={{
+                        height: 150,
+                        marginVertical: 5,
+                        flexDirection: 'row',
+                        overflow: 'hidden'
+                    }}
+                >   
+                    <Image
+                        style={{ 
+                            width: 150, 
+                            height: 150, 
+                            borderTopLeftRadius: 10,
+                            borderBottomLeftRadius: 10 
+                        }}
+                        source={{ uri: item.image }}
+                    />
+                    <View 
+                        style={{
+                            flex: 1,
+                            backgroundColor:  'rgb(36, 40, 47)',
+                            paddingLeft: 10,
+                            paddingTop: 10
+                        }}
+                    >
+                        <Text 
+                            style={{ 
+                                color: '#ffffff', 
+                                fontWeight: 'bold', 
+                                fontSize: 20
+                            }}
+                        >{ item.name }</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <View 
+                                style={{ 
+                                    backgroundColor: 'green', 
+                                    height: 10, 
+                                    width: 10,
+                                    borderRadius: 100,
+                                    marginTop: 5,
+                                    marginLeft: 1
+                                }} 
+                            />
+                            <Text style={{ color: '#ffffff', fontWeight: 'bold', fontSize: 13 }}>{ `  ${ item.status } - ${ item.species }` }</Text>
+                        </View>
+                        <Text style={{ color: 'rgb(158, 158, 158)', marginTop: 10 }}>Gender: { item.gender }</Text>
+                        <Text style={{ color: 'rgb(158, 158, 158)' }}>Created { moment( item.created , "YYYY-MM-DD").fromNow() }</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 15,  paddingRight: 10 }}>
+                            <Text style={{ color: '#ffffff', fontSize: 13, fontWeight: 'bold' }}>See all chapters</Text>
+                            {/* <Ionicons name="arrow-forward" size={ 20 } color="white" /> */}
+                        </View>
+                    </View>
+                </View> 
+            </TouchableOpacity>
+        )
+    }
 
     return (        
-        <ScrollView style={{ marginTop: top, marginHorizontal: 20 }}>
-            <Text style={{ fontSize: 25, fontWeight: 'bold' }}>Info API</Text>
-            <Button title="Back to Page 1" onPress={ () => navigation.pop() }/>
-            <Text>{ JSON.stringify( state ) }</Text>
-        </ScrollView>
+        <View 
+            style={{ 
+                flex: 1,
+                // backgroundColor: 'rgb(36, 40, 47)'
+            }}
+        >
+            <Text style={{ fontSize: 25, fontWeight: 'bold', marginTop: top + 10, marginLeft: 20 }}>Rick And Morty API</Text>
+            <FlatList 
+                style={{ marginTop: 20, marginLeft: 20, flex: 1 }}
+                data={ state }
+                renderItem={ renderItem }
+                keyExtractor={ item => item.id.toString() }
+                showsVerticalScrollIndicator={ false }
+            />
+        </View>
+
+       
     )
 }
